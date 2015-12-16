6e63069edae75468dca662aa83c067b365aff0d4
stcz19 <stefo.cruz94@gmail.com>
2015-12-16 08:36:19 -0800

    added some playlist

--- playlist.cpp
+++ playlist.cpp
@@ -1,5 +1,44 @@
 #include "playlist.h"
+#include <stdlib>     /* srand, rand */
+#include <ctime>       /* time */
 
-playList::playList()
+playlist::playlist()
 {
+
+}
+playlist:: addSongs()
+{
+
+}
+
+playlist:: deleteSongs()
+{
+
+}
+
+playlist:: display()
+{
+
+}
+
+playlist:: shuffle(bool yes)
+{
+
+}
+
+playlist:: loop(bool yes)
+{
+
+}
+
+playlist:: random(bool yes)
+{
+    srand(time(NULL));
+    int index = rand()% songs.size(); //create index from 0 - vector_size-1
+    if(yes)
+    {
+        //play the song with given index
+        //call the play function from engine
+        getPlayingOffset();
+    }
 }
